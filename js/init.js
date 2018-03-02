65ee43a3bcec335601a80874272c28bf2fb465a1
Alonzo Moses <alonzo.k.moses@gmail.com>
2018-03-02 14:13:14 -0500

    added new links and design tabs

--- js/init.js
+++ js/init.js
@@ -6,3 +6,11 @@
     $('.modal').modal()
   }) // end of document ready
 })(jQuery) // end of jQuery name space
+
+$(window).scroll(() => {
+  $('#sticky-nav').css('top', $(window).scrollTop())
+})
+
+setInterval(function () {
+  $('.carousel').carousel('next')
+}, 2000)
--- js/script.js
+++ js/script.js
@@ -1,7 +1,1 @@
-$(window).scroll(() => {
-  $('#sticky-nav').css('top', $(window).scrollTop())
-})
 
-setInterval(function () {
-  $('.carousel').carousel('next')
-}, 2000)
